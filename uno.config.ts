7555b6a1e67f8472f75e398618cd9c2b926976f2
Sorami Hisamoto <s@89.io>
2023-04-08 18:15:00 +0900

    prettier - tabWidth = 4

--- uno.config.ts
+++ uno.config.ts
@@ -1,51 +1,51 @@
 import {
-  defineConfig,
-  presetAttributify,
-  presetUno,
-  presetWebFonts,
+    defineConfig,
+    presetAttributify,
+    presetUno,
+    presetWebFonts,
 } from "unocss";
 import transformerDirectives from "@unocss/transformer-directives";
 import { extractorSvelte } from "@unocss/core";
 
 export default defineConfig({
-  extractors: [extractorSvelte],
-  shortcuts: [],
-  transformers: [transformerDirectives()],
-  presets: [
-    presetAttributify(),
-    presetUno(),
-    presetWebFonts({
-      provider: "google",
-      fonts: {
-        base: [
-          {
-            name: "New Tegomin",
-          },
-          {
-            name: "serif",
-            provider: "none",
-          },
-        ],
-        yuji: [
-          {
-            name: "Yuji Syuku",
-          },
-          {
-            name: "serif",
-            provider: "none",
-          },
-        ],
-        roman: [
-          {
-            name: "Inknut Antiqua",
-            weights: ["400"],
-          },
-          {
-            name: "serif",
-            provider: "none",
-          },
-        ],
-      },
-    }),
-  ],
+    extractors: [extractorSvelte],
+    shortcuts: [],
+    transformers: [transformerDirectives()],
+    presets: [
+        presetAttributify(),
+        presetUno(),
+        presetWebFonts({
+            provider: "google",
+            fonts: {
+                base: [
+                    {
+                        name: "New Tegomin",
+                    },
+                    {
+                        name: "serif",
+                        provider: "none",
+                    },
+                ],
+                yuji: [
+                    {
+                        name: "Yuji Syuku",
+                    },
+                    {
+                        name: "serif",
+                        provider: "none",
+                    },
+                ],
+                roman: [
+                    {
+                        name: "Inknut Antiqua",
+                        weights: ["400"],
+                    },
+                    {
+                        name: "serif",
+                        provider: "none",
+                    },
+                ],
+            },
+        }),
+    ],
 });
