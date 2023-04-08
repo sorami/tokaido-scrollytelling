2d711477769e4f627067b7b458b7e685746ef0b0
Sorami Hisamoto <s@89.io>
2023-04-09 00:41:49 +0900

    Map into a component

--- src/App.svelte
+++ src/App.svelte
@@ -1,98 +1,15 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import mapboxgl from "mapbox-gl";
-  import "mapbox-gl/dist/mapbox-gl.css";
+  import Map from "./Map.svelte";
   import Header from "./Header.svelte";
   import Colophon from "./Colophon.svelte";
   import Station from "./Station.svelte";
   import stationData from "./assets/stations.json";
-
-  mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
-
-  onMount(async () => {
-    const map = new mapboxgl.Map({
-      container: "map",
-      style: `https://api.maptiler.com/maps/jp-mierune-streets/style.json?key=${
-        import.meta.env.VITE_MAPTILER_KEY
-      }`,
-      center: [139.767, 35.681],
-      zoom: 9,
-      bearing: 0,
-      pitch: 60,
-      interactive: false,
-      projection: "globe",
-      transition: {
-        duration: 300,
-        delay: 0,
-      },
-    });
-
-    map.addControl(new mapboxgl.ScaleControl(), "bottom-right");
-    map.addControl(new mapboxgl.NavigationControl(), "bottom-left");
-    map.addControl(new mapboxgl.GeolocateControl(), "bottom-left");
-
-    const addStationLayers = () => {
-      map.addSource("station-src", {
-        type: "geojson",
-        data: "stations/points.geojson",
-      });
-
-      map.addLayer({
-        id: "station-circle",
-        type: "circle",
-        source: "station-src",
-        layout: {},
-        paint: {
-          "circle-color": "rgba(31,41,55,1)",
-          "circle-radius": [
-            "interpolate",
-            ["linear"],
-            ["zoom"],
-            4,
-            0,
-            8,
-            3,
-            10,
-            12,
-          ],
-          "circle-opacity": 1,
-        },
-      });
-
-      map.addLayer({
-        id: "station-label",
-        type: "symbol",
-        source: "station-src",
-        layout: {
-          "text-field": ["get", "no"],
-          "text-size": [
-            "interpolate",
-            ["linear"],
-            ["zoom"],
-            4,
-            0,
-            8,
-            3,
-            10,
-            12,
-          ],
-        },
-        paint: {
-          "text-color": "#fff",
-        },
-      });
-    };
-
-    map.on("load", () => {
-      addStationLayers();
-    });
-  });
 </script>
 
 <main>
-  <Header />
+  <Map />
 
-  <div id="map" />
+  <Header />
 
   <div id="station-container">
     {#each stationData as data}
@@ -104,13 +21,6 @@
 </main>
 
 <style>
-  #map {
-    top: 0;
-    height: 100vh;
-    width: 100vw;
-    position: fixed;
-  }
-
   #station-container {
     padding-top: 10vh;
     padding-bottom: 10vh;
---
+++ src/Map.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import mapboxgl from "mapbox-gl";
+  import "mapbox-gl/dist/mapbox-gl.css";
+
+  mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
+
+  onMount(async () => {
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: `https://api.maptiler.com/maps/jp-mierune-streets/style.json?key=${
+        import.meta.env.VITE_MAPTILER_KEY
+      }`,
+      center: [139.767, 35.681],
+      zoom: 9,
+      bearing: 0,
+      pitch: 60,
+      interactive: false,
+      projection: "globe",
+      transition: {
+        duration: 300,
+        delay: 0,
+      },
+    });
+
+    map.addControl(new mapboxgl.ScaleControl(), "bottom-right");
+    map.addControl(new mapboxgl.NavigationControl(), "bottom-left");
+    map.addControl(new mapboxgl.GeolocateControl(), "bottom-left");
+
+    const addStationLayers = () => {
+      map.addSource("station-src", {
+        type: "geojson",
+        data: "stations/points.geojson",
+      });
+
+      map.addLayer({
+        id: "station-circle",
+        type: "circle",
+        source: "station-src",
+        layout: {},
+        paint: {
+          "circle-color": "rgba(31,41,55,1)",
+          "circle-radius": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            4,
+            0,
+            8,
+            3,
+            10,
+            12,
+          ],
+          "circle-opacity": 1,
+        },
+      });
+
+      map.addLayer({
+        id: "station-label",
+        type: "symbol",
+        source: "station-src",
+        layout: {
+          "text-field": ["get", "no"],
+          "text-size": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            4,
+            0,
+            8,
+            3,
+            10,
+            12,
+          ],
+        },
+        paint: {
+          "text-color": "#fff",
+        },
+      });
+    };
+
+    map.on("load", () => {
+      addStationLayers();
+    });
+  });
+</script>
+
+<div id="map" />
+
+<style>
+  #map {
+    top: 0;
+    height: 100vh;
+    width: 100vw;
+    position: fixed;
+  }
+</style>
