995f70cffb6af4e98b0a3456b5ea1dd1a7bd2f86
Sorami Hisamoto <s@89.io>
2023-04-08 23:38:31 +0900

    add a tag attributes

--- src/Header.svelte
+++ src/Header.svelte
@@ -9,6 +9,8 @@
     ”東海道五十三次（とうかいどうごじゅうさんつぎ）は、江戸時代に整備された五街道の一つ、東海道にある53の宿場を指す。古来、道中には風光明媚な場所や有名な名所旧跡が多く、浮世絵や和歌・俳句の題材にもしばしば取り上げられた。”
     <br />
     <a
+      target="_blank"
+      rel="noopener noreferrer"
       href="https://ja.wikipedia.org/wiki/%E6%9D%B1%E6%B5%B7%E9%81%93%E4%BA%94%E5%8D%81%E4%B8%89%E6%AC%A1"
       >Wikipedia (ja)</a
     >
@@ -19,13 +21,18 @@
     the rest areas along the Tōkaidō, which was a coastal route that ran from
     Nihonbashi in Edo (modern-day Tokyo) to Sanjō Ōhashi in Kyoto."
     <br />
-    <a href="https://en.wikipedia.org/wiki/53_Stations_of_the_T%C5%8Dkaid%C5%8D"
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href="https://en.wikipedia.org/wiki/53_Stations_of_the_T%C5%8Dkaid%C5%8D"
       >Wikipedia (en)</a
     >
   </div>
 
   <div class="text-xs font-yuji mt-6">
-    <a href="https://sorami.dev">Sorami Hisamoto</a>, 2023.04
+    <a target="_blank" rel="noopener noreferrer" href="https://sorami.dev"
+      >Sorami Hisamoto</a
+    >, 2023.04
   </div>
 </div>
 
