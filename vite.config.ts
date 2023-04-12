9ed2e2f2d25fece0f8d5e3da4c454c7225669673
Sorami Hisamoto <s@89.io>
2023-04-12 21:34:51 +0900

    Update the vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "", // relative to the deployment directory
   plugins: [UnoCSS(), svelte()],
 });
